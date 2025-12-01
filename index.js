1b4447e4da7044a76074136259dbf4f2bea07e03
Your Name <you@example.com>
2025-12-01 00:10:18 -0300

    ADD Js examples

---
+++ index.js
@@ -0,0 +1,21 @@
+
+console.log ("Hello World")
+
+var a ="hola"
+console.log(a)
+a = "chau"
+console.log(a)
+if (true) {
+    console.log(a)
+}
+let b 
+console.log(b);
+b = 55
+console.log(b);
+if (true) {
+    console.log(b)
+}
+
+const c = 91
+console.log(c)
+console.log(c.toString())
